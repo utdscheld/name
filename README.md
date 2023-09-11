2067c5f50a8d00b2a2a4e30e0c87ed996758b4b1
lukewellsdev <luke@lukewells.dev>
2023-09-11 16:52:25 -0500

    Initial Commit - Hello World Pane

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# NAME
+# vsname extension
---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { HelloWorldPanel } from './HelloWorldPanel';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "testinghi" is now active!');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vsname2.helloWorld", () => {
+			HelloWorldPanel.createOrShow(context.extensionUri);
+		})
+	)
+
+	let disposable = vscode.commands.registerCommand('vsname2.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from vsname2');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
