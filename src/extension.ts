39238a6c1f8161ce107155dd28f2e325458d359b
hkwheat <83097237+hkwheat@users.noreply.github.com>
2023-09-25 20:39:42 -0500

    sends dap initialize message, hopefully

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as dap from '@vscode/debugadapter';
 import { HelloWorldPanel } from './HelloWorldPanel';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "testinghi" is now active!');
+
+	console.log(new dap.InitializedEvent());
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand("vsname2.helloWorld", () => {
